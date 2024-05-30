95d49bae95052b2f1b342adb3c662d9b61328210
JulijaTaranda <153869026+JulijaTaranda@users.noreply.github.com>
2024-05-30 17:27:14 +0300

    Update weather.php

--- weather.php
+++ weather.php
@@ -1,12 +1,11 @@
 <?php
 //cURL GET method
 // sources: https://openweathermap.org/api -> Current weather data
-// First - read API call, я выбрала -> Built-in API request by city name -> https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-// Second, make cURL шаблон for GET request
-//нам нужно Parameters: q, appid, mode, units, lang. Это написано под API call in documentation
-//My API key: b2ccafa3fce1a80a7385b887fd17c460 (from website, need to be registrated)
+// -> Built-in API request by city name -> https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+//Parameters: q, appid, mode, units, lang. Это написано под API call in documentation
 
-$url = 'http://api.openweathermap.org/data/2.5/weather'; //2.5 API version, parameter - weather. Это то, что идёт до ? в документации как вызвать АПИ и http без с
+
+$url = 'http://api.openweathermap.org/data/2.5/weather'; 
 
 $options = array(
     'q' => 'London',
@@ -16,21 +15,20 @@ $options = array(
 );
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //returntr.. значит, что ответ, который получит скрипт мы еще обработаем сами, что не надо его сразу выводить на страницу и можно поставить 1 можно тру
-// настраиваем урл куда будем стучаться
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 curl_setopt($ch, CURLOPT_URL, $url.'?'.http_build_query($options));
 
 $response = curl_exec($ch);
 
-//eto pishem posle togo, kak ostaljnoj kod napisali i viveli print_r dlja proverki
-$data = json_decode($response,true);//true чтоб создать массив из длинной нашей строки, которую видно в принт_р
-//теперь добавляем print_r($data);
+
+$data = json_decode($response,true);
+//print_r($data); for test
 
 curl_close($ch);
 
 echo '<pre>';
 print_r($response);
-print_r($data); // теперь можно закомментировать или убрать принт_р(респонс)
+print_r($data);
 
 
 ?>
